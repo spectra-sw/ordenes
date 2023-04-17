18306841537bc5736ab0951aeb04eae7c98bdfe2
David Guerra <davidguerra@Davids-iMac.local>
2023-04-17 14:33:16 -0500

    20230417

--- resources/views/tablaproyecto.blade.php
+++ resources/views/tablaproyecto.blade.php
@@ -26,12 +26,12 @@
         @if ($p->director ==0)
           <td></td>
         @else
-          <td>{{ $p->ndirector->nombre . " " . $p->ndirector->apellido1 }}</td>
+          <td>{{ $p->ndirector ? $p->ndirector->nombre . " " . $p->ndirector->apellido1 : '' }}</td>
         @endif
         @if ($p->lider ==0)
           <td></td>
         @else
-          <td>{{ $p->nlider->nombre . " ". $p->nlider->apellido1 }}</td>
+          <td>{{ $p->nlider ? $p->nlider->nombre . " ". $p->nlider->apellido1 : ''}}</td>
         @endif
         
         <td>{{ $p->ciudad }}</td>
